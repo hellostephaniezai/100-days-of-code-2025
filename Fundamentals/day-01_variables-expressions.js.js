69a78d5e4c916fc6b75fbb269a4500e44fe5821e
stephaniezai <jupitergalilean4@gmail.com>
2025-06-11 23:50:47 +0100

    Day 1: practiced variables and expressions

---
+++ Fundamentals/day-01_variables-expressions.js.js
@@ -0,0 +1,13 @@
+// My JavaScript variables
+
+let name = "Stephanie";
+console.log("Hi, my name is " + "name");
+
+let apples = 4;
+let oranges = 3;
+let totalFruits = apples + oranges;
+console.log("You have " + totalFruits + "fruits");
+
+let mood = "tired";
+mood = "focused";
+console.log("Right now, I'm feeling " + mood);
