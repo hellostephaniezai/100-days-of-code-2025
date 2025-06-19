c130a024f8ffaabdf637156aebfc717d22803d3c
stephaniezai <jupitergalilean4@gmail.com>
2025-06-19 11:51:31 +0100

    4/100 Practiced if/else conditions & added a codewar kata i  did and tested

--- Fundamentals/day-04_if-else.js
+++ Fundamentals/day-04_if-else.js
@@ -1,6 +1,33 @@
 
 // If/Else - Conditionals 
 
+// Time conditions
+let time = 8;
 
+if (time < 9) {
+    console.log('you are on time');
+}
 
-  
+// Tempreture conditions
+let tempreture = 30;
+
+if (tempreture > 24){
+    console.log('Its hot out there!');
+} else {
+    console.log('Its alright out there');
+}
+
+//Codewars Kata - make value negative 
+
+function makeNegative(num){
+    if (num < 0) {
+        return num;
+    } else {
+        return num * -1;
+    }
+}
+
+//tests 
+console.log(makeNegative(-2))
+console.log(makeNegative(6))
+console.log(makeNegative(0))
