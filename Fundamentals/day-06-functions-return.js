3d3059b15cde1e6aaa186a74e6dc2f67fcdea3d2
stephaniezai <jupitergalilean4@gmail.com>
2025-06-27 21:49:47 +0100

    6/100 – Functions with return values, kg/lbs converters, custom reminder

--- Fundamentals/day-06-functions-return.js
+++ Fundamentals/day-06-functions-return.js
@@ -2,10 +2,46 @@
 
 //Return a calculation 
 
-
+// Part 1: 
 function multiply (a,b){
 return a * b;
 }
 
 let result = multiply (3,4);
 console.log(result);
+
+
+// Part 2:  Return a String 
+
+
+function createGreeting(name, timeOfDay){
+        return "Good " + timeOfDay + ", " + name + "! ";
+}
+
+let greeting = createGreeting("Stephanie", "evening");
+console.log(greeting); 
+
+
+function remindMe(name, theMessage,){
+    return "Hey! " + theMessage + ", " + name + "!"
+}
+
+let reminder = remindMe("Stephanie", "Keep Going")
+console.log(reminder)
+
+// Part 3:  Return a conversion 
+
+function toPounds(kg){
+    return kg * 2.20462;
+}
+
+let weightInPounds = toPounds(60);
+console.log(weightInPounds);
+
+
+function toKilograms(lbs){
+    return lbs / 2.20462;
+}
+
+let weightToKilograms = toKilograms(145);
+    console.log(weightToKilograms);
