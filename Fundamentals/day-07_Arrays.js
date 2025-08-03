f7b33e303f78c70d4a53cb86e0d484266763806f
stephaniezai <jupitergalilean4@gmail.com>
2025-08-03 23:22:26 +0100

    7/100 – Arrays: filtered sweet fruits, used .filter() on objects

--- Fundamentals/day-07_Arrays.js
+++ Fundamentals/day-07_Arrays.js
@@ -1,5 +1,24 @@
 // Day 7 - Arrays
 
+//Part 1:
 
-let fruits = ["Kiwi", "Apple", "Melon"];
-console.log(fruits[0 + 2])
+let fruits = ["Kiwi", "Apple", "Melon", "Orange"];
+console.log(fruits[1])
+console.log(fruits[ 3])
+
+
+//Part 2:
+
+let allFruits = [
+    {name: "Kiwi", sweet: false },
+    {name: "Mango", sweet: true },
+    {name: "Lemon", sweet: false },
+    {name: "Strawberry", sweet: true },
+    {name: "coconut", sweet: false },
+];
+
+let sweetOnly = allFruits.filter(fruit => fruit.sweet === true);
+console.log(sweetOnly)
+
+let notSweet = allFruits.filter(fruit => fruit.sweet === false);
+console.log(notSweet)
