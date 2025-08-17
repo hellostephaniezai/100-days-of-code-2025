0ebb35658b0cd803a1334ee6ec0b4e78390e5b1a
stephaniezai <jupitergalilean4@gmail.com>
2025-08-17 23:02:09 +0100

    Practiced Moreloops with arrays and counting

--- Fundamentals/day-03_loops.js
+++ Fundamentals/day-03_loops.js
@@ -21,3 +21,32 @@ for (let CountStrt = 0; CountStrt <= 20; CountStrt+= 3) {
      //Runs a count and skip 3
     console.log("Count Up:" + CountStrt)
 }
+
+//Practiced Sun 17 Aug
+let shopList = [" chick", "beef", "lamb"];
+
+for ( let i = 0; i < shopList.length; i++) {
+    console.log(`BBQ buy: ${shopList[i]}`);
+}
+
+let iceCream = ["Pistachio", "Mango", "Coconut","Bubble Gum"];
+
+for (let i = 0; i < iceCream.length; i++) {
+    console.log(`My Favourite Ice Cream is : ${iceCream[i]}`);
+}
+
+let dogBreed = ["Cavapoos", "Golden Retrievers", "Dobermans" ]
+
+for (let i = 0; i < dogBreed.length; i++){
+    console.log(`I love Dog Breeds like: ${dogBreed[i]}`)
+}
+
+for (let numberplot = 0; numberplot <= 30; numberplot = numberplot + 5) {
+    console.log(numberplot);
+}
+
+let myCars = ["BMW i4","Mercedes GL","VW Amarok "]
+
+for (let i = 0; i < myCars.length; i++){
+    console.log(`I own a: ${myCars[i]}`);
+}
